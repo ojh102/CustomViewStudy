ee5bdd1928cee7f73daf8f4c5065adbc3be0f52a
OH JAE HWAN <ojh102@naver.com>
2016-06-22 10:22:28 +0900

    Initial commit

---
+++ app/src/main/java/com/ojh/customviewstudy/ClearEditText.java
@@ -0,0 +1,115 @@
+package com.ojh.customviewstudy;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
+
+/**
+ * Created by JaeHwan Oh on 2016-06-22.
+ */
+public class ClearEditText extends AppCompatEditText implements TextWatcher, OnTouchListener, OnFocusChangeListener {
+
+    private Drawable clearDrawable;
+    private OnFocusChangeListener onFocusChangeListener;
+    private OnTouchListener onTouchListener;
+
+    public ClearEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        Drawable tempDrawable = ContextCompat.getDrawable(getContext(), R.drawable.abc_ic_clear_mtrl_alpha);
+        clearDrawable = DrawableCompat.wrap(tempDrawable);
+        DrawableCompat.setTintList(clearDrawable,getHintTextColors());
+        clearDrawable.setBounds(0, 0, clearDrawable.getIntrinsicWidth(), clearDrawable.getIntrinsicHeight());
+
+        setClearIconVisible(false);
+
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+
+        addTextChangedListener(this);
+    }
+
+    private void setClearIconVisible(boolean visible) {
+        clearDrawable.setVisible(visible, false);
+        setCompoundDrawables(null,null, visible ? clearDrawable : null,null);
+    }
+
+    @Override
+    public void setOnFocusChangeListener( OnFocusChangeListener onFocusChangeListener) {
+        this.onFocusChangeListener = onFocusChangeListener;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+
+        if (onFocusChangeListener != null) {
+            onFocusChangeListener.onFocusChange(v, hasFocus);
+        }
+    }
+
+    @Override
+    public void setOnTouchListener( OnTouchListener onTouchListener) {
+        this.onTouchListener = onTouchListener;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int x = (int) event.getX();
+        if (clearDrawable.isVisible() && x > getWidth() - getPaddingRight() - clearDrawable.getIntrinsicWidth()) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                setError(null);
+                setText(null);
+            }
+            return true;
+        }
+
+        if (onTouchListener != null) {
+            return onTouchListener.onTouch(v, event);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public final void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+        if (isFocused()) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+}
